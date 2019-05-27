a3d93f572311c7ebbdd6156088a9bbab273418fe
zhiyong.dang <zhiyong.dang@ck-telecom.com>
2019-05-27 02:23:14 -0700

    compile aarch64 kernel

--- code/linuxkernel/build_os.sh
+++ code/linuxkernel/build_os.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+DEBUG="true"
 LINUX_KERNEL_TAR="linux-4.4.179.tar.xz"
-ENABLE_CHECK_SUM="false"
+FORCE_CHECK_SUM="false"
 if [ ! -f ${LINUX_KERNEL_TAR} ];then
     wget https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/${LINUX_KERNEL_TAR}
 else
-    if [ ${ENABLE_CHECK_SUM} == "true" ];then
+    if [ ${FORCE_CHECK_SUM} == "true" ];then
         KERNEL_SUMS="sha256sums.asc"
         rm -rf ${KERNEL_SUMS}
         wget https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/${KERNEL_SUMS}
@@ -16,9 +17,24 @@ else
     fi
 fi
 FORCE_EXTRACT_TAR="false"
+LINUX_KERNEL_DIR=${LINUX_KERNEL_TAR%.tar.xz}
 if [ ${FORCE_EXTRACT_TAR} == "true" ];then
-    rm -rf ${LINUX_KERNEL_TAR%.tar.xz}
+    rm -rf ${LINUX_KERNEL_DIR}
 fi
-if [ ! -d ${LINUX_KERNEL_TAR%.tar.xz} ];then
+if [ ! -d ${LINUX_KERNEL_DIR} ];then
     tar xvf ${LINUX_KERNEL_TAR}
 fi
+ARCH="arm64"
+if [ ${ARCH} == "arm64" ];then
+    CROSS_COMPILE="aarch64-linux-gnu-"
+fi
+cd ${LINUX_KERNEL_DIR}
+FORCE_REBUILD="false"
+if [ ${FORCE_REBUILD} == "true" ];then
+    make distclean
+fi
+if [ ${DEBUG} == "true" ];then
+    echo "Compile "${ARCH}" kernel with "${CROSS_COMPILE}"gcc"
+fi
+make ARCH=${ARCH} defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
